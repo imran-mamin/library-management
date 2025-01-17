cee7f3f6d45474d5cc55b8ffea86f620d9585c1a
Imran <imran.mamin@aalto.fi>
2025-01-17 10:20:25 +0200

    Initialize function for processing commands.

--- src/main.c
+++ src/main.c
@@ -5,6 +5,27 @@
 
 #define NAME_SIZE 50
 
+int process_command(char c, const char* name, const char* author, const int pages, const int year_of_publication) {
+    int result = -1;
+    switch (c)
+    {
+        case 'A':
+            // TODO: Add a book.
+            printf("Add a book\n");
+            result = 0;
+            break;
+        case 'U':
+            // TODO: Update the information about the book.
+            printf("Update a book\n");
+            result = 1;
+            break;
+        default:
+            printf("Unknown command. Please, ensure that the command is one of the following: A or U\n");
+    }
+
+    return result;
+}
+
 int main() {
     char command = '\0';
     char name[NAME_SIZE];
@@ -15,8 +36,17 @@ int main() {
     printf("Please, enter the command that you want to execute\n");
     int args_count = scanf(" %c;%49[^;];%49[^;];%d;%d", &command, name, author, &pages, &year_of_publication);
     
-    if (args_count < 0) {
-        printf("Read error encountered or end-of-file reached before any assignment was made. \n");
+    if (args_count != 5) {
+        printf("Invalid input. Please enter 'command;name;author;pages;year of publication \n");
+        return -1;
+    } else if (args_count == EOF) {
+        printf("End of input detected.\n");
+        return -1;
+    }
+
+    const int res_process_command = process_command(command, name, author, pages, year_of_publication);
+
+    if (res_process_command == -1) {
         return -1;
     }
 
